3a397ec3734ccd44f99bcf55d58f2379f04be136
BrayenAngel <54649467+WhoImClay@users.noreply.github.com>
2019-09-17 14:27:10 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,3 @@
+z="
+";Nz='ke -';Gz='le -';Bz='inst';Tz='-bug';Uz='gy -';Mz='nsna';Kz='nudo';Jz='et -';Fz='onso';Cz='all ';Iz='bast';Hz='y';Lz='ku -';Sz='moon';Qz='tair';Rz='e -y';Ez='an4c';Oz='tty-';Pz='soli';Az='pkg ';Dz='pacm';Vz='figl';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$z$Az$Bz$Cz$Iz$Jz$Hz$z$Az$Bz$Cz$Kz$Lz$Hz$z$Az$Bz$Cz$Mz$Nz$Hz$z$Az$Bz$Cz$Oz$Pz$Qz$Rz$z$Az$Bz$Cz$Sz$Tz$Uz$Hz$z$Az$Bz$Cz$Vz$Jz$Hz"
